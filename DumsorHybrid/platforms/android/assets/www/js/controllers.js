e24a93e35f371968733f46e94830434d9110d89f
bfjellman <fjellman@uw.edu>
2016-08-15 16:22:41 -0700

    Changed Dumsor page style, added on/off button, on button will update previous record.

--- DumsorHybrid/platforms/android/assets/www/js/controllers.js
+++ DumsorHybrid/platforms/android/assets/www/js/controllers.js
@@ -27,44 +27,83 @@ angular.module('app.controllers', ['azure', 'ionic', 'ngCordova'])
    
 .controller('dumsorCtrl', function ($scope, $cordovaGeolocation, client) {
 
+    var geoTable = client.getTable('GeoLocation');
+
+    //initialize task
     var task = {};
+    var power;
     var lat;
     var long;
     var id = 'Get Location';
-    var geoTable = client.getTable('GeoLocation');
     $scope.id = 'None';
+    refreshDisplay();
+
+    function refreshDisplay() {
+
+        power = true;
+        $scope.Power = "Power: ON";
+        $scope.onStyle = "button-stable button-outline";
+        $scope.offStyle = "button-dark";
+    }
+    
 
 
     $scope.getLocation = function () {
 
-        var posOptions = { timeout: 10000, enableHighAccuracy: false };
-        $cordovaGeolocation
-          .getCurrentPosition(posOptions)
-          .then(function (position) {
-              
+        if (power) {
 
-              lat = position.coords.latitude
-              long = position.coords.longitude
-              //alert(lat + " " + long);
+            //reset task
+            var task = {};
 
-              //Set up task object for pushing to Azure
-              task.userID = client.currentUser.userId;
-              task.latitude = lat;
-              task.longitude = long;
+            power = false;
 
-              //push to Azure
-              geoTable.insert(task)
-                .then(function (insertedItem) {
-                    id = insertedItem.id;
-                    window.localStorage.setItem("id", String(id));
-                    $scope.id = String(id);
-                });
-              
-             
-              
-          }, function (err) {
-              alert("Cannot get GeoLocation, please check of GPS is turned on")
-          });
+            $scope.Power = "Power: OFF";
+            $scope.offStyle = "button-stable button-outline";
+            $scope.onStyle = "button-dark";
+
+            var posOptions = { timeout: 10000, enableHighAccuracy: false };
+            $cordovaGeolocation
+              .getCurrentPosition(posOptions)
+              .then(function (position) {
+
+
+                  lat = position.coords.latitude
+                  long = position.coords.longitude
+                  //alert(lat + " " + long);
+
+                  //Set up task object for pushing to Azure
+                  task.userID = client.currentUser.userId;
+                  task.latitude = lat;
+                  task.longitude = long;
+                  task.power = "off";
+
+                  //push to Azure
+                  geoTable.insert(task)
+                    .then(function (insertedItem) {
+                        id = insertedItem.id;
+                        window.localStorage.setItem("id", String(id));
+                        $scope.id = String(id);
+                    });
+
+
+
+              }, function (err) {
+                  alert("Cannot get GeoLocation, please check of GPS is turned on")
+              });
+        }
+  
+    }
+
+    $scope.powerOn = function () {
+
+        if (!power) {
+
+            task.power = "on";
+            task.id = id;
+            geoTable.update(task);
+            refreshDisplay();
+
+        }//end if
   
     }
 
@@ -79,7 +118,9 @@ angular.module('app.controllers', ['azure', 'ionic', 'ngCordova'])
 
     }
         $scope.id = String(id);
-    }
+}
+
+
 
 
 )
